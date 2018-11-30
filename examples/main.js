069884b249e9a2bd899dc68e8d950c24ef6a1aa2
EC2 Default User <ec2-user@ip-172-31-41-241.us-east-2.compute.internal>
2018-11-30 07:35:01 +0000

    Updated to the newest aframe

--- examples/main.js
+++ examples/main.js
@@ -3,6 +3,6 @@ import extras from 'aframe-extras';
 import keyboardControls from 'aframe-keyboard-controls';
 import clickDragComponent from '../src/index';
 
-extras.physics.registerAll(aframe);
-aframe.registerComponent('keyboard-controls', keyboardControls);
+// extras.physics.registerAll(aframe);
+// aframe.registerComponent('keyboard-controls', keyboardControls);
 clickDragComponent(aframe);
